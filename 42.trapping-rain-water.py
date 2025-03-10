c961202a2bfcff29ce5782d3d9f46342d82d95e7
FauricioTec <fagranados@estudiantec.cr>
2025-03-10 02:17:57 -0600

    Add solution for trapping rain water problem

---
+++ 42.trapping-rain-water.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode id=42 lang=python3
+#
+# [42] Trapping Rain Water
+#
+# https://leetcode.com/problems/trapping-rain-water/description/
+#
+# algorithms
+# Hard (64.30%)
+# Likes:    33564
+# Dislikes: 586
+# Total Accepted:    2.7M
+# Total Submissions: 4.1M
+# Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+#
+# Given n non-negative integers representing an elevation map where the width
+# of each bar is 1, compute how much water it can trap after raining.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array
+# [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section)
+# are being trapped.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# n == height.length
+# 1 <= n <= 2 * 10^4
+# 0 <= height[i] <= 10^5
+# 
+# 
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
+        water = 0
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                water += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                water += maxR - height[r]
+        return water
+    
+print(Solution().trap([5,5,1,7,1,1,5,2,7,6]))
+        
+# @lc code=end
+
